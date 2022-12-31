d96614200006d523ce392281b6cf87b89b193788
Alexander Castillo <acastillo@asanteit.com.ar>
2022-12-31 02:31:44 -0300

    CRUD Finished

--- videojuegoapp/forms.py
+++ videojuegoapp/forms.py
@@ -3,17 +3,80 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from videojuegoapp.models import *
-
+COLOR_CHOICES = (
+    ('green','GREEN'),
+    ('blue', 'BLUE'),
+    ('red','RED'),
+    ('orange','ORANGE'),
+    ('black','BLACK'),
+)
 
 class UserForm (UserCreationForm):
     first_name = forms.CharField(
-    max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
-    email = forms.EmailField(max_length=50,widget=forms.EmailInput(attrs={'class':'form-control'}))
-    username = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control'}))
-    password1 = forms.CharField (label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField (label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    
-    class Meta:
-        model = User
-        fields = ['first_name','last_name','email','username' , 'password1', 'password2']
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=50, widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+        label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+
+
+class CreateFormGenero (forms.Form):
+    nombre = forms.CharField(max_length=40)
+
+
+class UpdateFormGenero (forms.Form):
+    nombre = forms.CharField(max_length=40)
+    fecha_creacion = forms.DateField()
+
+
+class FormPlataforma (forms.Form):
+    nombre = forms.CharField(max_length=40)
+    fecha_lanzamiento = forms.DateField()
+
+
+class FormDesarrollador (forms.Form):
+    nombre = forms.CharField(max_length=100)
+    fundacion = forms.DateField()
+    web_url = forms.CharField(max_length=255)
+
+
+class FormAutor (forms.Form):
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=40)
+    fecha_nacimiento = forms.DateField()
+    foto_url = forms.CharField(max_length=255)
+
+class FormPost (forms.Form):
+    titulo = forms.CharField(max_length=40)
+    subtitulo = forms.CharField(max_length=40)
+    contenido = forms.CharField(widget=forms.Textarea)
+    imagen_url = forms.CharField(max_length=255)
+    autor = forms.ChoiceField(choices=[])
+    categoria = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        super(FormPost, self).__init__(*args, **kwargs)
+        self.fields['autor'].choices = [(x.pk, x.nombre) for x in Autor.objects.all().order_by("nombre")]
+        self.fields['categoria'].choices = [(x.pk, x.nombre) for x in Categoria.objects.all().order_by("nombre")]
+
+class FormVideojuego (forms.Form):
+    nombre = forms.CharField(max_length=255)
+    genero = forms.ChoiceField(choices=[])
+    desarrollador = forms.ChoiceField(choices=[])
+    fecha_lanzamiento = forms.DateField()
+    plataformas = forms.MultipleChoiceField(choices=[], widget=forms.CheckboxSelectMultiple)
+   
+    def __init__(self, *args, **kwargs):
+        super(FormVideojuego, self).__init__(*args, **kwargs)
+        self.fields['genero'].choices = [(x.pk, x.nombre) for x in Genero.objects.all().order_by("nombre")]
+        self.fields['desarrollador'].choices = [(x.pk, x.nombre) for x in Desarrollador.objects.all().order_by("nombre")]
+        self.fields['plataformas'].choices = [(x.pk, x.nombre) for x in Plataforma.objects.all().order_by("nombre")]
+
+class FormCategoria (forms.Form):
+    nombre = forms.CharField(max_length=40)
--- videojuegoapp/urls.py
+++ videojuegoapp/urls.py
@@ -9,5 +9,42 @@ urlpatterns = [
     path('auth/register', register, name='gameblog-register'),
     path('auth/login', login, name='gameblog-login'),
     path('auth/profile', profile, name='gameblog-profile'),
-    path('auth/logout', LogoutView.as_view(next_page="/home"), name='gameblog-logout')
+    path('auth/logout', LogoutView.as_view(next_page="/home"), name='gameblog-logout'),
+    # Admin
+    path('actions', actions, name='gameblog-actions'),
+    # CRUD Genero
+    path('actions/create/genero', create_genero, name='gameblog-create-genero'),
+    path('actions/list/genero/<int:page>', list_genero, name='gameblog-list-genero'),
+    path('actions/update/genero/<int:id>', update_genero, name='gameblog-update-genero'),
+    path('actions/delete/genero/<int:id>', delete_genero, name='gameblog-delete-genero'),
+    # CRUD Plataforma
+    path('actions/create/plataforma', create_plataforma, name='gameblog-create-plataforma'),
+    path('actions/list/plataforma/<int:page>', list_plataforma, name='gameblog-list-plataforma'),
+    path('actions/update/plataforma/<int:id>', update_plataforma, name='gameblog-update-plataforma'),
+    path('actions/delete/plataforma/<int:id>', delete_plataforma, name='gameblog-delete-plataforma'),
+    # CRUD Desarrollador
+    path('actions/create/desarrollador', create_desarrollador, name='gameblog-create-desarrollador'),
+    path('actions/list/desarrollador/<int:page>', list_desarrollador, name='gameblog-list-desarrollador'),
+    path('actions/update/desarrollador/<int:id>', update_desarrollador, name='gameblog-update-desarrollador'),
+    path('actions/delete/desarrollador/<int:id>', delete_desarrollador, name='gameblog-delete-desarrollador'),
+    # CRUD Autor
+    path('actions/create/autor', create_autor, name='gameblog-create-autor'),
+    path('actions/list/autor/<int:page>', list_autor, name='gameblog-list-autor'),
+    path('actions/update/autor/<int:id>', update_autor, name='gameblog-update-autor'),
+    path('actions/delete/autor/<int:id>', delete_autor, name='gameblog-delete-autor'),
+    # CRUD Post
+    path('actions/create/post', create_post, name='gameblog-create-post'),
+    path('actions/list/post/<int:page>', list_post, name='gameblog-list-post'),
+    path('actions/update/post/<int:id>', update_post, name='gameblog-update-post'),
+    path('actions/delete/post/<int:id>', delete_post, name='gameblog-delete-post'),
+    # CRUD Categoria
+    path('actions/create/categoria', create_categoria, name='gameblog-create-categoria'),
+    path('actions/list/categoria/<int:page>', list_categoria, name='gameblog-list-categoria'),
+    path('actions/update/categoria/<int:id>', update_categoria, name='gameblog-update-categoria'),
+    path('actions/delete/categoria/<int:id>', delete_categoria, name='gameblog-delete-categoria'),
+    # CRUD Videojuego
+    path('actions/create/videojuego', create_videojuego, name='gameblog-create-videojuego'),
+    path('actions/list/videojuego/<int:page>', list_videojuego, name='gameblog-list-videojuego'),
+    path('actions/update/videojuego/<int:id>', update_videojuego, name='gameblog-update-videojuego'),
+    path('actions/delete/videojuego/<int:id>', delete_videojuego, name='gameblog-delete-videojuego'),
 ]
